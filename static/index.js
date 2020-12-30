fa77c2758ad2fb3f8abb9e9b78c7f90dc1ddeead
BorissowT <timsregisters@gmail.com>
2020-12-30 15:30:53 +0300

    Add some elementary input validation on the template

--- static/index.js
+++ static/index.js
@@ -2,13 +2,22 @@ const reader = new FileReader();
 
 $("#submit").on("click",()=>{
   const file = document.getElementById('pictureInput').files[0];
-
+  if(!file){
+    alert("choose a picture");
+    location.reload();
+  }
   //file's loaded
   reader.addEventListener("load", function () {
     var result = reader.result.split(",");
-    var width = $('#width').val()
-    var height = $("#height").val()
-    send_ajax_with_image(result[1], width, height);
+    var width = $('#width').val();
+    var height = $("#height").val();
+    if (Number.isInteger(parseInt(width)) && Number.isInteger(parseInt(height)) && width > 0 && height > 0) {
+      send_ajax_with_image(result[1], width, height);
+    }
+    else{
+      alert("width and height has to be postitive numbers");
+      location.reload();
+    }
   }, false);
 
   function send_ajax_with_image(image, width, height){
@@ -22,7 +31,7 @@ $("#submit").on("click",()=>{
                 },
       data: json
     }).done(function(data, textStatus, request) {
-      console.log(request.getResponseHeader('id'))
+      console.log("Operation's id is:", request.getResponseHeader('id'))
     }).fail(function(e) {
       console.log(e);
     });
