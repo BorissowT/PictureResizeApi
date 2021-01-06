fdef74bcd02148902b704fc33d0ff55b288909e1
BorissowT <timsregisters@gmail.com>
2021-01-06 18:42:06 +0300

    Configure consumer's connection to local mysql server

--- kafka_client/consumer_container/db_consumer/database.py
+++ kafka_client/consumer_container/db_consumer/database.py
@@ -1,22 +1,31 @@
 import os
 
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import mysql.connector
 from sqlalchemy import create_engine
 
+import time
 
-db_user = os.environ.get("DB_MYSQL_USER")
+db_user = os.environ.get("DB_MYSQL_REMOTE_USER")
 db_pass = os.environ.get("DB_MYSQL_PASS")
-db_port = 3306
-db_host = "localhost"
+db_address = os.environ.get("DB_MYSQL_ADDRESS")
 db_name = "pictureapi_mydb"
+print(db_user, db_pass)
+flag = True
+while flag:
+    try:
+        connection = "mysql+mysqlconnector://{0}:{1}@{2}/{3}".format(db_user, db_pass, db_address, db_name)
+        engine = create_engine(connection)
 
-connection = "mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(db_user, db_pass, db_host, db_port, db_name)
-engine = create_engine(connection)
+        Base = automap_base()
+        Base.prepare(engine, reflect=True)
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
-
-Response = Base.classes.response
-session = Session(engine)
+        Response = Base.classes.response
+        session = Session(engine)
+    except Exception as e:
+        print(e)
+        time.sleep(1.5)
+    else:
+        flag = False
--- kafka_client/consumer_container/serialization_consumer/schema.py
+++ kafka_client/consumer_container/serialization_consumer/schema.py
@@ -1,7 +1,6 @@
 from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemySchema
-
-from kafka_client.consumer_container.db_consumer.database import Response
+from db_consumer.database import Response
 
 
 class ResponseSchema(SQLAlchemySchema):
