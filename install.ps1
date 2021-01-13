a05b727ba0c9a0dadef886c20ea06f32dde0e38f
BorissowT <timsregisters@gmail.com>
2021-01-13 22:13:27 +0300

    Add powershell script

---
+++ install.ps1
@@ -0,0 +1,26 @@
+﻿function all_in_one{
+Write-Output "YES";
+cd .\kafka_container;
+docker-compose up -d;
+cd ..;
+cd .\db_container;
+docker-compose up -d;
+cd ..;
+cd .\api_container;
+docker-compose up -d;
+cd ..;
+cd .\consumer_container;
+docker-compose up -d;
+cd ..;
+}
+
+$start = Read-Host 'Want to start all of parts of this application on this machine? (Y|N)';
+while($start -ne 'Y' -and $start -ne 'N'){
+$start = Read-Host 'Want to start all of parts of this application on this machine? (Y|N)';
+}
+if ($start -ieq 'Y'){
+all_in_one;
+}
+elseif($start -ieq 'N'){
+Write-Output "NO";
+}
