a46b314f0ad323dfb2beb2a5184cd3615e7f7c57
BorissowT <timsregisters@gmail.com>
2021-01-13 15:48:04 +0300

    Add new name

---
+++ api_container/app.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from flask import Flask
+
+from service_functions import set_app_params
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+    app_args = set_app_params(sys.argv)
+    debug = app_args["debug"]
+    host = app_args["host"]
+    from views import *
+    app.run(debug=debug, host=host)
---
+++ api_container/service_functions.py
@@ -0,0 +1,28 @@
+import hashlib
+import time
+import os
+
+topic_name = "topic_test"
+
+
+def hash_id(ip_address):
+    time_of_request = time.time()
+    id_concatenated = ip_address + str(time_of_request)
+    hashed_id = hashlib.sha1(id_concatenated.encode('utf-8')).hexdigest()
+    return hashed_id
+
+
+def set_app_params(argv):
+    debug = True
+    host = "localhost"
+    for arg in argv:
+        if arg == "-container":
+            host = "0.0.0.0"
+            debug = False
+            global topic_name
+            topic_name = os.environ.get("KAFKA_TOPIC_NAME")
+        if "-topic_name=" in arg:
+            topic_name = arg.split("-topic_name=")[1]
+    return {"debug": debug, "host": host}
+
+
