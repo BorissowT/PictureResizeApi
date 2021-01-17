63560239508856a6e1a8f405f64394fbf3a12728
BorissowT <timsregisters@gmail.com>
2021-01-17 16:36:07 +0300

    Add manual credentials

--- install.ps1
+++ install.ps1
@@ -21,6 +21,29 @@ Out-File -Encoding utf8 -FilePath .\consumer_container\.env -InputObject $conten
 
 }
 
+function fill_manual_credentials{
+
+$pass_var_secured = Read-Host -assecurestring "Please enter your data-base password";
+$topic_name_var = Read-Host 'Enter kafka topic name (note, that you can add and delete new topics in control-center interface)';
+$group_name_var = Read-Host 'Enter kafka-consumer group name';
+
+$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass_var_secured)
+$pass_var = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+
+$mysql_pass = "DB_MYSQL_PASS={0}" -f $pass_var;
+$kafka_topic_name = "KAFKA_TOPIC_NAME={0}" -f $topic_name_var;
+$kafka_group_name = "KAFKA_GROUP_NAME={0}" -f $group_name_var;
+
+$content = "{0}" -f $mysql_pass;
+Out-File -Encoding utf8 -FilePath .\db_container\.env -InputObject $content ;
+
+$content = "{0} `n{1} `n{2}" -f $mysql_pass, $mysql_root, $kafka_topic_name;
+Out-File -Encoding utf8 -FilePath .\api_container\.env -InputObject $content ;
+
+$content = "{0} `n{1} `n{2} `n{3}" -f $mysql_pass, $mysql_root, $kafka_topic_name, $kafka_group_name;
+Out-File -Encoding utf8 -FilePath .\consumer_container\.env -InputObject $content;
+}
+
 function fill_env_credentials {
 
 $credentials_command = Read-Host  "Want to use default credentials? (Y|N)";
@@ -28,7 +51,7 @@ if ($credentials_command -ieq 'Y'){
 fill_def_credentials;
 }
 elseif($credentials_command -ieq 'N'){
-Write-Output "NO";
+fill_manual_credentials;
 }
 
 }
@@ -39,18 +62,18 @@ Write-Output "YES";
 
 fill_env_credentials;
 
-cd .\kafka_container;
-docker-compose up -d;
-cd ..;
-cd .\db_container;
-docker-compose up -d;
-cd ..;
-cd .\api_container;
-docker-compose up -d;
-cd ..;
-cd .\consumer_container;
-docker-compose up -d;
-cd ..;
+#cd .\kafka_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\db_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\api_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\consumer_container;
+#docker-compose up -d;
+#cd ..;
 }
 
 #start
