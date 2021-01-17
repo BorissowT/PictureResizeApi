0caf77e3e5b1d7c95b8f26fd57349f1c3711d20e
BorissowT <timsregisters@gmail.com>
2021-01-17 15:46:43 +0300

    Fix bugs. Fix output of credentials

--- install.ps1
+++ install.ps1
@@ -5,7 +5,10 @@ $mysql_pass = 'DB_MYSQL_PASS=12345';
 $kafka_topic_name = 'KAFKA_TOPIC_NAME=topic_test';
 $kafka_group_name = 'KAFKA_GROUP_NAME=my-group-id';
 
+$credentials_output_flag = $false;
+
 function fill_def_credentials {
+$global:credentials_output_flag = $true;
 
 $content = "{0}" -f $mysql_pass;
 Out-File -Encoding utf8 -FilePath .\db_container\.env -InputObject $content ;
@@ -21,10 +24,10 @@ Out-File -Encoding utf8 -FilePath .\consumer_container\.env -InputObject $conten
 function fill_env_credentials {
 
 $credentials_command = Read-Host  "Want to use default credentials? (Y|N)";
-if ($start -ieq 'Y'){
+if ($credentials_command -ieq 'Y'){
 fill_def_credentials;
 }
-elseif($start -ieq 'N'){
+elseif($credentials_command -ieq 'N'){
 Write-Output "NO";
 }
 
@@ -36,18 +39,18 @@ Write-Output "YES";
 
 fill_env_credentials;
 
-cd .\kafka_container;
-docker-compose up -d;
-cd ..;
-cd .\db_container;
-docker-compose up -d;
-cd ..;
-cd .\api_container;
-docker-compose up -d;
-cd ..;
-cd .\consumer_container;
-docker-compose up -d;
-
+#cd .\kafka_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\db_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\api_container;
+#docker-compose up -d;
+#cd ..;
+#cd .\consumer_container;
+#docker-compose up -d;
+#cd ..;
 }
 
 #start
@@ -60,8 +63,10 @@ $start = Read-Host 'Want to start all parts of this application on this machine?
 
 if ($start -ieq 'Y'){
 all_in_one;
+if($credentials_output_flag){
 $output =  "`nYour credentials: `n{0} `n{1} `n{2} `n{3}"-f $mysql_pass, $mysql_root, $kafka_topic_name, $kafka_group_name;
 Write-Output $output;
+}
 $end = Read-Host 'Press "enter" to close';
 }
 
