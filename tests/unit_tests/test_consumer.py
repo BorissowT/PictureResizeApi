a36c45ac1bacc253064c4ce41be6c47fbd975193
BorissowT <timsregisters@gmail.com>
2021-01-28 15:23:35 +0300

    Add resizing picture test(doesn't work )

--- tests/unit_tests/test_consumer.py
+++ tests/unit_tests/test_consumer.py
@@ -2,7 +2,10 @@ import unittest
 
 from consumer_container.serialization_consumer.schema import response_schema
 from consumer_container.db_consumer.database import session
-# add consumer_container. to schema
+from consumer_container.service_functions.serv_fun import resize_picture
+
+# !!!
+# add consumer_container. imports to schema.py, serv_fun.py
 
 
 class ResponseConsumerSerializationTest(unittest.TestCase):
@@ -76,3 +79,24 @@ class ResponseConsumerSerializationTest(unittest.TestCase):
         self.assertFalse(response_schema.validate(data, session=session),
                          msg="Request schema doesn't indicate too big height")
 
+
+# class ConsumerResizingFunctionalityTest(unittest.TestCase):
+#
+#     resized_image = None
+#     tested_data = None
+#     maxDiff = None
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         identifier = "some_test_identifier"
+#         with open('tests/unit_tests/test_data/base64_unresized_picture.txt', 'r') as file:
+#             image = file.read().replace('\n', '')
+#         with open('tests/unit_tests/test_data/base64_100x100_resized_picture.txt', 'r') as file:
+#             cls.resized_image = file.read().replace('\n', '')
+#         width = 2
+#         height = 2
+#         cls.tested_data = {"image": image, "identifier": identifier, "width": width, "height": height}
+#
+#     def test_if_resizes_properly(self):
+#         resize_picture(self.tested_data)
+#         self.assertEqual(str(self.resized_image), str(self.tested_data["image"]))
