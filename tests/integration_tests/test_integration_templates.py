5fd2889d7e2188e57b8d1f04dec30a6bc4021b6b
BorissowT <timsregisters@gmail.com>
2021-02-02 17:20:37 +0300

    Refactor tests

--- tests/integration_tests/test_integration_templates.py
+++ tests/integration_tests/test_integration_templates.py
@@ -1,20 +1,17 @@
 from api_container.views import app
 import unittest
 
+
 # add api_container. to imports in tested folder(schema, app, database, views)
 
 
 class IndexTemplateTest(unittest.TestCase):
     url = "/"
 
-    def test_success_code(self):
+    def test_template(self):
         tester = app.test_client(self)
         response = tester.get(self.url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_content_type(self):
-        tester = app.test_client(self)
-        response = tester.get(self.url)
+        self.assertEqual(response.status_code, 200) and \
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
 
 
