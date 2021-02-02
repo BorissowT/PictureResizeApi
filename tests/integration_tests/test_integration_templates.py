32c2180e0a822eb99cb978f19a2dc8bbaf2c3752
BorissowT <timsregisters@gmail.com>
2021-02-02 16:34:53 +0300

    Add templates tests

---
+++ tests/integration_tests/test_integration_templates.py
@@ -0,0 +1,26 @@
+from api_container.views import app
+import unittest
+
+# add api_container. to imports in tested folder(schema, app, database, views)
+
+
+class IndexTemplateTest(unittest.TestCase):
+    url = "/"
+
+    def test_success_code(self):
+        tester = app.test_client(self)
+        response = tester.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_content_type(self):
+        tester = app.test_client(self)
+        response = tester.get(self.url)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+
+class ResultTemplateTest(IndexTemplateTest):
+    url = "result?id="
+
+
+if __name__ == "__main__":
+    unittest.main()
