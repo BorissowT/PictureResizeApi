eef67549aeacd8375e3bb21384e436e36910f215
BorissowT <timsregisters@gmail.com>
2021-02-02 18:45:18 +0300

    Add 404 template test

--- tests/integration_tests/test_integration_templates.py
+++ tests/integration_tests/test_integration_templates.py
@@ -7,16 +7,23 @@ import unittest
 
 class IndexTemplateTest(unittest.TestCase):
     url = "/"
+    code = 200
 
     def test_template(self):
         tester = app.test_client(self)
         response = tester.get(self.url)
-        self.assertEqual(response.status_code, 200) and \
+        self.assertEqual(response.status_code, self.code) and \
         self.assertEqual(response.content_type, "text/html; charset=utf-8")
 
 
 class ResultTemplateTest(IndexTemplateTest):
     url = "result?id="
+    code = 200
+
+
+class NotFoundTemplateTest(IndexTemplateTest):
+    url = "/1"
+    code = 404
 
 
 if __name__ == "__main__":
